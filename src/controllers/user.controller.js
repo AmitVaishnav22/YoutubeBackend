93d8d1474a0e039ee7b3abcb6586563402770a55
AmitVaishnav22 <vaishnanamit@gmail.com>
2024-12-25 23:28:10 +0530

    Fix formatting in updateUserCoverImage response

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -264,7 +264,7 @@ const updateUserCoverImage=asyncHandler(async(req,res)=>{
 
     return res.status(200)
               .json(new apiResponse(200,user,"coverImage successfully uploaded"))
-})
+})  
 
 
 
