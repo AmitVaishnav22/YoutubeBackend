79a1ede942e24a62ab2ea7cad7411ae706bef6fd
AmitVaishnav22 <vaishnanamit@gmail.com>
2025-01-05 15:13:07 +0530

    Fix import statement: correct casing in community router import and remove trailing spaces in app.js

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(cors({
 app.use(express.json({limit:'16kb'}))
 app.use(express.urlencoded({extended:true,limit:'16kb'}))
 app.use(express.static("public"))
-app.use(cookieParser())
+app.use(cookieParser())    
 
 
 //routes
@@ -24,7 +24,7 @@ import playlistRouter from './routes/playlist.route.js'
 import commentRouter from './routes/comment.route.js'
 import likeRouter from './routes/like.route.js'
 import dashboardRouter from './routes/dashboard.route.js'
-import communityRouter from './routes/Community.route.js' 
+import communityRouter from './routes/community.route.js' 
 import memberShipRouter from './routes/membership.route.js'
 import healthCheckRouter from './routes/healthcheck.route.js'
 
@@ -32,7 +32,7 @@ app.use("/api/v1/users",userRouter)
 app.use("/api/v1/subscriptions",subcriptionRouter)
 app.use("/api/v1/videos",videoRouter)
 app.use("/api/v1/playlists",playlistRouter)
-app.use("/api/v1/comment",commentRouter)
+app.use("/api/v1/comment",commentRouter)   
 app.use("/api/v1/like",likeRouter)
 app.use("/api/v1/dashboard",dashboardRouter)
 app.use("/api/v1/community",communityRouter)
